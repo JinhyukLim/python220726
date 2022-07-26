2769b125f2ff32d239cde73225cd6b4af781c11b
JinhyukLim <ij_hyuk@naver.com>
2022-07-26 14:53:31 +0900

    람다까지

--- function3.py
+++ function3.py
@@ -1,5 +1,8 @@
 # function3.py
 # 기본값이 있는 경우
+from unittest import result
+
+
 def times(a=10, b=20):
     return a*b
 
@@ -16,3 +19,34 @@ def connectURI(server, port):
 # 호출
 print(connectURI("credu.com", "80"))
 print(connectURI(port="80", server="credu.com"))
+
+# 가변인자
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+# 호출
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
+
+# 정의되지 않은 인자(필수와 옵션이 있는 경우)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+# 호출
+print(userURIBuilder("credu.com", "80", id="kim"))
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234"))
+
+# 람다함수(이름이 없는 간결한 함수 정의)
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+print( (lambda x:x*x)(3) )
+print( globals() )
+
