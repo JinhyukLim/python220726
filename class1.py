6ae2e9739eeaaeccda339acebde8184891ee430c
JinhyukLim <ij_hyuk@naver.com>
2022-07-26 16:26:46 +0900

    클래습 연습2

--- class1.py
+++ class1.py
@@ -5,12 +5,15 @@ class Person:
     def __init__(self):
         self.name = "default name"
     #인스턴스 메서드
-    def print(self)
+    def print(self):
         print("My name is {0}".format(self.name))
 
+# 내어쓰기(shift+tab), 들여쓰기(tab)
 # 2) 인스턴스 행성
 p1 = Person()
-
+p2 = Person()
+p1.name = "전우치"
 # 3) 메서드 호출
 p1.print()
+p2.print()
 
