592a3b9e2976cd21e87a971210e17f39aa0eadb3
JinhyukLim <ij_hyuk@naver.com>
2022-07-28 15:34:59 +0900

    판다스 시작

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -7,6 +7,7 @@ import re
 # User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+f = open("c:\\work\\clien.txt.", "wt", encoding="utf-8")
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
@@ -32,6 +33,10 @@ for n in range(0,10):
                         # print(title.strip())
                         if (re.search('아이폰', title)):
                                 print(title.strip())
+                                f.write(title.strip() + "\n")
                 except:
                         pass
+
+printf("웹 크롤링 종료")
+f.close()
         
