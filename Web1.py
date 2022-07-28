d592837832c78cc93cc0fa8dd1a4889c16007859
JinhyukLim <ij_hyuk@naver.com>
2022-07-28 11:52:10 +0900

    크롤링 연습1

---
+++ Web1.py
@@ -0,0 +1,10 @@
+# Web1.py
+# 웹문서를 검색(패키지는 폴더형태)
+from bs4 import BeautifulSoup
+
+# 페이지 로딩
+page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
+# 검색이 용이한 객체\
+soup = BeautifulSoup(page, "html.parser")
+print(soup.prettify())
+
